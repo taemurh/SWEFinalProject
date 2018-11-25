472266c5b65e8eaf417f30b75c9c5639352bf37a
Manuel Marroquin <mmarroqu@nd.edu>
2018-11-24 19:00:06 -0500

    Main GUI
    Main GUI

--- src/se2018/SWEFinalProject/GUI/ScrumBoard.java
+++ src/se2018/SWEFinalProject/GUI/ScrumBoard.java
@@ -1,5 +1,26 @@
 package se2018.SWEFinalProject.GUI;
 
-public class ScrumBoard {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class ScrumBoard extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("scrum_board.fxml"));
+		primaryStage.setTitle("Scrum Board Tool");
+		primaryStage.setScene(new Scene(root, 1200, 800));
+		primaryStage.show();
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+		
 }
