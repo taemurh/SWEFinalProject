ea9dae49af11e4b32350fad9855834317aa59828
Manuel Marroquin <mmarroqu@nd.edu>
2018-11-29 23:10:36 -0500

    Added button functunality to bring up additional menus

--- src/se2018/SWEFinalProject/GUI/FXMLController.java
+++ src/se2018/SWEFinalProject/GUI/FXMLController.java
@@ -1,21 +1,57 @@
 package se2018.SWEFinalProject.GUI;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class FXMLController {
     @FXML 
     protected void handleAddStoryButtonAction(ActionEvent event) {
-        System.out.println("Add Story button pressed!");
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("add_story_window.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Add Story!");
+            stage.setScene(new Scene(root, 450, 700));
+            stage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     @FXML 
     protected void handleBacklogButtonAction(ActionEvent event) {
-        System.out.println("Backlog button pressed!");
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("burndown_window.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Backlog");
+            stage.setScene(new Scene(root, 700, 700));
+            stage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     @FXML 
     protected void handleBurndownButtonAction(ActionEvent event) {
-        System.out.println("Burndown button pressed!");
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("burndown_window.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Burndown Chart");
+            stage.setScene(new Scene(root, 800, 700));
+            stage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
