b1b847e1fd2cdf99bd48b76bdd6e085a36ee12a7
Paul Kwak <pkwak1@nd.edu>
2018-12-10 16:23:25 -0500

    update functionality added..no test though yet cause no button

--- src/se2018/SWEFinalProject/Client/FXMLDocumentController.java
+++ src/se2018/SWEFinalProject/Client/FXMLDocumentController.java
@@ -327,6 +327,49 @@ public class FXMLDocumentController implements Initializable {
     protected void handleStoryClick(ActionEvent event) {
     	
     }
+
+    @FXML 
+    protected void handleStoryUpdate(ActionEvent event) {
+    	// get id from story fxml doc
+    	// once id is grabbed, get story
+    	try {
+    		FXMLLoader loader = new FXMLLoader(getClass().getResource("Story_Details.fxml"));
+            /*
+            root = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Story Details");
+            stage.setScene(new Scene(root, 450, 300));
+            stage.show();
+            */
+           
+            StoryController controller = loader.getController();
+            int id = Integer.parseInt(controller.IDField.getText());
+            Story story = gateway.getStory(id);
+            
+            // then setFields
+            controller.displayAuthorField.setText(story.getAuthor());
+            controller.displayTitleField.setText(story.getTitle());
+            controller.displayPointsField.setText(Integer.toString(story.getStoryPoints()));
+            controller.displayDescriptionField.setText(story.getDescription());
+         
+        	// then sendStory
+            // Story story = new Story(0, authorField.getText(), titleField.getText(), descriptionField.getText(), Integer.parseInt(pointsField.getText()));
+	    	String storyJSON = "";
+
+	    	try {
+	    		storyJSON = story.toString_();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	
+	      	gateway.sendStory(storyJSON);
+            
+    	} catch (Exception d) {
+            d.printStackTrace();
+        }
+    	
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
