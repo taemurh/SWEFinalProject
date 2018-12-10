20dd012a983727a704e54dd70af88bc23f62a18a
Manuel Marroquin <mmarroqu@nd.edu>
2018-12-10 16:00:32 -0500

    Added field for ID

--- src/se2018/SWEFinalProject/Client/StoryController.java
+++ src/se2018/SWEFinalProject/Client/StoryController.java
@@ -3,9 +3,14 @@ package se2018.SWEFinalProject.Client;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
 
 public class StoryController {
 	@FXML TextField displayAuthorField;
+	@FXML ComboBox statusDropDown;
+	@FXML Label IDField;
 	@FXML TextField displayPointsField;
 	@FXML TextField displayTitleField;
 	@FXML TextArea displayDescriptionField;
